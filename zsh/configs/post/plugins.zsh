6ccbc8488ef3071f93b702d337b986f1007b67fe
Caio Bianchi <caio@caio.ca>
2022-05-17 19:57:05 -0400

    zsh plugins config

--- zsh/configs/post/plugins.zsh
+++ zsh/configs/post/plugins.zsh
@@ -1,14 +1,13 @@
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
-# source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source /usr/local/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh
-source /usr/local/share/zsh-history-substring-search/zsh-history-substring-search.zsh
-fpath=(/usr/local/share/zsh-completions $fpath)
+source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /usr/local/share/zsh-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+# source /usr/local/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+# source /usr/local/share/zsh-history-substring-search/zsh-history-substring-search.zsh
+# fpath=(/usr/local/share/zsh-completions $fpath)
 
 if type brew &>/dev/null; then
   FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
 fi
 
-. /usr/local/opt/asdf/asdf.sh
 eval "$(starship init zsh)"
