fadfe68abc272a166851d2381e2fbb44933fad37
Caio Bianchi <caio@caio.ca>
2022-07-20 13:22:58 -0400

    Modify functions

--- zsh/configs/post/plugins.zsh
+++ zsh/configs/post/plugins.zsh
@@ -2,9 +2,7 @@
 
 source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /usr/local/share/zsh-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
-# source /usr/local/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh
-# source /usr/local/share/zsh-history-substring-search/zsh-history-substring-search.zsh
-# fpath=(/usr/local/share/zsh-completions $fpath)
+source /usr/local/etc/profile.d/z.sh
 
 if type brew &>/dev/null; then
   FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
