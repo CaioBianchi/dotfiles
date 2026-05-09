4a1176025324a6da2138e4484e1a15b64c1337e5
Caio Bianchi <caio@caio.ca>
2026-05-09 17:48:12 -0400

    Set up 1p + add ghostty configs

--- fish/conf.d/10-env.fish
+++ fish/conf.d/10-env.fish
@@ -8,3 +8,9 @@ set -gx RUBY_ZJIT_ENABLE true
 
 set -gx DO_NOT_TRACK 1
 set -gx HOMEBREW_NO_ANALYTICS 1
+set -gx OP_ENVIRONMENT_ID drpaj7cws44nj2yhrr3mjaupli
+
+# Load ~/.env if it exists
+if test -f ~/.env
+    envsource ~/.env
+end
---
+++ fish/functions/envsource.fish
@@ -0,0 +1,6 @@
+function envsource --description 'Load environment variables from .env file'
+    for line in (cat $argv | grep -v '^#' | grep -v '^$')
+        set item (string split -m 1 '=' $line)
+        set -gx $item[1] $item[2]
+    end
+end
