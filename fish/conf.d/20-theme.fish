5e95df9594b50da0f30c3ed6c7a5b170788df04b
Caio Bianchi <caio@caio.ca>
2026-05-16 13:47:11 -0400

    Add dynamic theming to fish

--- fish/conf.d/20-theme.fish
+++ fish/conf.d/20-theme.fish
@@ -2,5 +2,12 @@
 # Theme & Prompt
 # ================================================================
 
-fish_config theme choose tokyonight_storm
+# Detect macOS appearance mode
+set appearance (defaults read -g AppleInterfaceStyle 2>/dev/null)
+
+if test "$appearance" = Dark
+    fish_config theme choose tokyonight_storm >/dev/null
+else
+    fish_config theme choose tokyonight_day >/dev/null
+end
 set -U tide_character_vi_icon_default '❯'
