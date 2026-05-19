07fd1231df2ac154aad6deb404421d6056705e5f
Caio Bianchi <caio@caio.ca>
2026-05-19 02:00:48 -0400

    Add ripgrep config

--- fish/conf.d/10-env.fish
+++ fish/conf.d/10-env.fish
@@ -6,6 +6,7 @@ set -gx EDITOR nvim
 set -gx VISUAL nvim
 set -gx RUBY_ZJIT_ENABLE true
 set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx RIPGREP_CONFIG_PATH $HOME/.config/rg/config
 
 set -gx DO_NOT_TRACK 1
 set -gx HOMEBREW_NO_ANALYTICS 1
