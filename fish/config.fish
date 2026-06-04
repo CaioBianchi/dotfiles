c3fd4f6d13f4ca844bf70c279aeadc21a9d011d7
Caio Bianchi <caio@caio.ca>
2026-06-03 23:31:16 -0400

    Remove heavy stuff

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,3 @@
 
 # All heavy lifting is now in conf.d/ files (loaded automatically)
 # Add only things that must run last if needed
-
-# >>> grok installer >>>
-fish_add_path $HOME/.grok/bin
-# <<< grok installer <<<
